aaf5b16cc27640c342c473b0cd80636e4926be6d
masterShanto <mastershanto@gmail.com>
2023-08-03 20:09:32 +0600

    1.0_assignment_M1

--- 2.2_typeConvartion_M1.dart
+++ 2.2_typeConvartion_M1.dart
@@ -1,4 +1,9 @@
 void  main(){
+var myList=[1,2,3];
+myList.remove(2);
+myList.add(5);
+print(myList);
 
-
+/*var map={"name":"jhon","shanto":30}
+print(map["name"]);*/
 }
---
+++ Assignments_Dart/1.0_Assignment_M1.dart
@@ -0,0 +1,50 @@
+void main(){
+
+  /// Declaring the variables and printing
+  const a=9, b=5;
+  print("a = ${a}, b = ${b}");
+
+  /// Addition and printing
+  int addition=a+b;
+  print("Addition: ${a} + ${b} = ${addition}");
+
+  /// Subtraction and printing
+  int subtraction=a-b;
+  print("Subtraction: ${a} - ${b} = ${subtraction}");
+
+  /// Multiplication and printing
+  int multiplication=a*b;
+  print("Multiplication: ${a} * ${b} = ${multiplication}");
+
+  /// Division and printing
+  double division=a/b;
+  print("Division: ${a} / ${b} = ${division}");
+
+  /// Modulus and printing
+  int modulus=a%b;
+  print("Modulus: ${a} % ${b} = ${modulus}");
+
+
+///......................OR......................///
+/*
+
+  const a=9, b=5;
+
+  int addition=a+b;
+  int subtraction=a-b;
+  int multiplication=a*b;
+  double division=a/b;
+  int modulus=a%b;
+
+  print("a = ${a}, b = ${b}");
+  print("Addition: ${a} + ${b} = ${addition}");
+  print("Subtraction: ${a} - ${b} = ${subtraction}");
+  print("Multiplication: ${a} * ${b} = ${multiplication}");
+  print("Division: ${a} / ${b} = ${division}");
+  print("Modulus: ${a} % ${b} = ${modulus}");
+
+*/
+
+
+
+}
