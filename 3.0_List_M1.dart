f9e51a45f6c80a52b3178729e278d82e12f6ca3f
masterShanto <mastershanto@gmail.com>
2023-08-03 13:09:45 +0600

    no comment

--- 3.0_List_M1.dart
+++ 3.0_List_M1.dart
@@ -1,9 +1,26 @@
 void main(){
+ ///Statically dataTypes Variable
+ String name="Sheikh Ajijul Hoque Shanto";
+ int ID=322586;
+ double CGPA=3.30;
+
+///Compound dataTypes Variable
+ var anyDataType=1.5;
+ // var random="name"; /// So var is not support it's value after importing 1st value
+ dynamic random= "Shanto";
+ random="Rahim";
+ random="Rafat"; /// So dynamic support all type of changing for it's value;
+
+
  const int numberOfSchool=23;
  /*numberOfSchool=35;
  numberOfSchool=20;
- numberOfSchool=015;*/
+ numberOfSchool=015;
+*/
 
- final String name;
+ final String companyName;
+ companyName="Bangladesh Programmer community";
+ // companyName="Ostad Flatform"; /// This is not acceptable for "final Keywork"
+
 
 }
